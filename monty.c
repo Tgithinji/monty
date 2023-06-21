86f27066b51b70f3ec600509ccb3cb196eba37de
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-06-21 13:16:03 +0300

    Check if user gave a file

--- monty.c
+++ monty.c
@@ -10,11 +10,15 @@
 int main(int argc, char **argv)
 {
 	(void) argv;
+	int fd;
 	/* check if number of arguments passed is correct */
 	if (argc != 2)
 	{
 		dprintf(2, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	/* open the provided file */
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
 	return (0);
 }
--- monty.h
+++ monty.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list implementation of
